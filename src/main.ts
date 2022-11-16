48ceaf93c2f4e077dc963f716ed8e34a3560cc1d
Leonardo Araujo <leonardo.santos@loupen.com.br>
2022-11-16 16:17:46 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:3002',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, *',
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await app.listen(4000);
 }
